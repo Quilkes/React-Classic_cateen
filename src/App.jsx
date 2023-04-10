9f27f8f255b3c85340f7ef7c00b86f76007dc315
banniroy <matinadekola33@gmail.com>
2023-04-10 12:21:26 +0100

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './Header'
+import Home from './Home'
+
+function App() {
+
+  return (
+    <main className="App">
+      <Header />
+      <Home />
+      
+    </main>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='bg-emerald-500'>
+            <section className='flex'>
+                <h1>Brytr Kitchen</h1>
+            </section>
+            <button>
+                <div>
+
+                </div>
+            </button>
+        </header>
+    )
+}
+
+export default Header
