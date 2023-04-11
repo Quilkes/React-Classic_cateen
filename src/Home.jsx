e3c6d250fe3a80761c20e2e32cda788d1417971b
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-04-11 17:39:19 +0100

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <main className='min-h-screen flex flex-col bg-slate-300 overflow-hidden'>
-      <div className='relative h-96 opacity-95'>
-         <section className= 'bg-slate-400 relative  h-[350px] w-[350px] rounded-full left-[-170px] top-[-145px]'>
+      <div className='relative h-[27.3rem] opacity-95'>
+         <section className= 'bg-slate-400 relative  h-[800px] w-[800px] rounded-full left-[-530px] top-[-57vh]'>
           <img className='absolute' src="./img/pics-1.png" alt="" />
           <img className='absolute' src="./img/pics-2.png" alt="" />
           <img className='absolute' src="./img/pics-3.jpeg" alt="" />
