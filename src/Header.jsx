7c38de2e33b90fdb034f35f89b78833b4e0a0f13
banniroy <matinadekola33@gmail.com>
2023-04-11 16:54:02 +0100

    commmit

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <header className='h-16 bg-gradient-to-r from-gray-300 to-gray-400 z-50 sticky top-0 opacity-95'>
             <section className='flex justify-between align-middle flex-row-reverse p-4 '>
                 <div></div>
-                <h1 className='text-white font-bold font-sans text-lg tracking-widest'> <span className='text-purple-700'>Brytr</span> <span className='font-mono'>Kitchen</span></h1>
+                <h1 className='text-white font-bold font-sans text-xl tracking-widest'> <span className='text-purple-700'>Brytr</span> <span className='font-mono'>Kitchen</span></h1>
 
                 <button className='relative outline-none h-8 w-11 flex justify-center items-center'>
                     <div className='absolute h-1 w-8 bg-slate-950 rounded '></div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <main className='min-h-screen flex flex-col bg-slate-300 overflow-hidden'>
-      <div className='relative h-96'>
-         <section className='relative bg-slate-400 h-800 w-800 rounded-full right-52 bottom-56'>
+      <div className='relative h-96 opacity-95'>
+         <section className= 'bg-slate-400 relative  h-[350px] w-[350px] rounded-full left-[-170px] top-[-145px]'>
           <img className='absolute' src="./img/pics-1.png" alt="" />
           <img className='absolute' src="./img/pics-2.png" alt="" />
           <img className='absolute' src="./img/pics-3.jpeg" alt="" />
