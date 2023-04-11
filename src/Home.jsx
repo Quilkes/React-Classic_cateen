52433fc43c18cd3950fb6b5e0d655baaa0522aa9
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-04-11 12:55:44 +0100

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <main className='min-h-screen flex flex-col bg-slate-300 overflow-hidden'>
       <div className='relative h-96'>
-        <section className='relative bg-slate-400 h-full w-full rounded-full right-52 bottom-56'>
+         <section className='relative bg-slate-400 h-800 w-800 rounded-full right-52 bottom-56'>
           <img className='absolute' src="./img/pics-1.png" alt="" />
           <img className='absolute' src="./img/pics-2.png" alt="" />
           <img className='absolute' src="./img/pics-3.jpeg" alt="" />
