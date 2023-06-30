ef52154704c958c79fb403422c1cbf28a9f60086
Banniroy <matinadekola33@gmail.com>
2023-06-30 09:48:30 +0100

    new update

--- src/components/MenuList-components/Menu.jsx
+++ src/components/MenuList-components/Menu.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import MenuPostFeeds from './MenuPostFeeds'
 import SkeletonDisplayProduct from '../Skeleton-Loading/SkeletonDisplayProduct'
-import Loading from '../Loading-state/Loading'
 
 const Menu = ({ menuListPosts, isLoading, handleAddProduct }) => {
   return (
     <section>
       {isLoading
-        ? <SkeletonDisplayProduct />
+        ? Array.from({ length: 3 }).map(() => (
+          [...Array(1)].map((_, i) => <SkeletonDisplayProduct key={i} />)
+        ))
         : <MenuPostFeeds
           menuListPosts={menuListPosts}
           handleAddProduct={handleAddProduct}
