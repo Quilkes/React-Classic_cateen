972183b781c5fb706a27181d940f6a1baf4f40ef
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-07-06 13:03:35 +0100

    Update MenuPostPage.jsx

--- src/components/MenuList-components/MenuPostPage.jsx
+++ src/components/MenuList-components/MenuPostPage.jsx
@@ -9,30 +9,33 @@ const notify = () => toast('here is your toast.');
 
 const MenuPostPage = ({ postMapped, handleAddProduct }) => {
   return (
-      <Link to=''>
-        <div className="meal-for-display">
-          <img
-            src={postMapped.image_path}
-            alt={postMapped.name}
-            height="200"
-            width="200"
-          />
+    <Link to={`/viewproduct/${postMapped.id}`}>
+      <div className="meal-for-display">
+        <img
+          src={postMapped.image_path}
+          alt={postMapped.name}
+          height="200"
+          width="200"
+        />
+        <br />
+        <section className="portions">
+          <h3>{postMapped.name}</h3>
           <br />
-          <section className="portions">
-            <h3>{postMapped.name}</h3>
-            <br />
-            <div className='button-container'>
-              <button className="order-now" onClick={notify}>Order now</button>
-              <Toaster />
-              <button
-                className="font"
-                onClick={() => handleAddProduct(postMapped)}>
-                <FontAwesomeIcon icon={faShoppingBasket} />
-              </button>
-            </div>
-          </section>
-        </div>
-      </Link>
+          <div className="button-container">
+            <button className="order-now" onClick={notify}>
+              Order now
+            </button>
+            <Toaster />
+            <button
+              className="font"
+              onClick={() => handleAddProduct(postMapped)}
+            >
+              <FontAwesomeIcon icon={faShoppingBasket} />
+            </button>
+          </div>
+        </section>
+      </div>
+    </Link>
   )
 }
 
