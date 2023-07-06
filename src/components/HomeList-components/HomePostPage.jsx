89d59a8424a82ea410d2ccea57f67eb9317556b7
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-07-06 13:04:53 +0100

    Update HomePostPage.jsx

--- src/components/HomeList-components/HomePostPage.jsx
+++ src/components/HomeList-components/HomePostPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../css/Home.css'
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons'
 import toast, { Toaster } from 'react-hot-toast';
@@ -8,31 +9,35 @@ const notify = () => toast('here is your toast.');
 
 const HomePostPage = ({ postMapped, handleAddProduct }) => {
   return (
+    <Link to={`/viewproduct/${postMapped.id}`}>
       <section className="meal-section">
         <div className="meal-for-display">
           <img
             src={postMapped.image_path}
             alt={postMapped.name}
-            height='200'
-            width='200'
+            height="200"
+            width="200"
           />
           <br />
           <section className="portions">
             <h3>{postMapped.name}</h3>
             <br />
-            <div className='button-container'>
-              <button className="order-now" onClick={notify} >Order now</button>
-              <Toaster />
+            <div className="button-container">
+              <button className="order-now" onClick={notify}>
+                Order now
+              </button>
               <button
                 className="font"
-                onClick={() => handleAddProduct(postMapped)}>
+                onClick={() => handleAddProduct(postMapped)}
+              >
                 <FontAwesomeIcon icon={faShoppingBasket} />
               </button>
             </div>
           </section>
         </div>
       </section>
-  )
+    </Link>
+  );
 }
 
 export default HomePostPage
