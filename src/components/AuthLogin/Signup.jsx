2ab74bff3e6cd287ce8bf161548a34d71ec4e6d9
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-07-07 14:29:19 +0100

    Update Signup.jsx

--- src/components/AuthLogin/Signup.jsx
+++ src/components/AuthLogin/Signup.jsx
@@ -1,75 +1,77 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { auth } from '../../firebase/firebaseSDK';
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import './Login.css';
 
 const Signup = () => {
+  const userRef = useRef();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
 
-    const userRef = useRef();
-    const navigate = useNavigate();
-    const location = useLocation();
-    const from = location.state?.from?.pathname || "/";
+  const [registerEmail, setRegisterEmail] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+  const [error, setError] = useState(null); // State for storing the error message
 
-    const [registerEmail, setRegisterEmail] = useState('');
-    const [RegisterPassword, setRegisterPassword] = useState('');
-
-    const signUp = async (e) => {
-        e.preventDefault();
-        try {
-            createUserWithEmailAndPassword(auth, email, password)
-            await userCredential;
-            navigate(from, { replace: true });
-        } catch (error) {
-
-        }
+  const signUp = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+      setRegisterEmail('');
+      setRegisterPassword('');
+      navigate(from, { replace: true });
+    } catch (error) {
+      setError('Error creating an account. Please try again.'); // Set the error message
     }
+  }
 
-    return (
-        <section>
-            <form onSubmit={signUp} className='login-form'>
-                <h2>Sign Up</h2>
-                <section className="username-container">
-                    <label htmlFor="email">User Email</label>
-                    <input
-                        type="text"
-                        name="email"
-                        id="email"
-                        ref={userRef}
-                        placeholder='Your Email'
-                        value={registerEmail}
-                        required
-                        onChange={e => setRegisterEmail(e.target.value)}
-                    />
-                </section>
-
-                <section className="password-container">
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        placeholder='Password'
-                        value={RegisterPassword}
-                        required
-                        onChange={e => setRegisterPassword(e.target.value)}
-                    />
-                </section>
-
-                <button type="submit">Create account</button>
-            </form>
-
-            <section>
-                <p>
-                    Already registered?
-                    <Link className='signup-terms' >Login in</Link>
-                </p>
-                <p>
-                    By registering, i accept your <Link className='signup-terms' to="/terms_condition">Terms and conditions.</Link>
-                </p>
-            </section>
+  return (
+    <section>
+      <form onSubmit={signUp} className='login-form'>
+        <h2>Sign Up</h2>
+        {error && <p className="error-message">{error}</p>} {/* Display error message if it exists */}
+        <section className="username-container">
+          <label htmlFor="email">User Email</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            ref={userRef}
+            placeholder='Your Email'
+            value={registerEmail}
+            required
+            onChange={e => setRegisterEmail(e.target.value)}
+          />
         </section>
-    )
+
+        <section className="password-container">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder='Password'
+            value={registerPassword}
+            required
+            onChange={e => setRegisterPassword(e.target.value)}
+          />
+        </section>
+
+        <button type="submit">Create account</button>
+      </form>
+
+      <section>
+        <p>
+          Already registered?
+          <Link className='signup-terms'>Login in</Link>
+        </p>
+        <p>
+          By registering, I accept your <Link className='signup-terms' to="/terms_condition">Terms and conditions.</Link>
+        </p>
+      </section>
+    </section>
+  )
 }
 
-export default Signup
+export default Signup;
