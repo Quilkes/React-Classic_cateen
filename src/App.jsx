dbd0cb33253034f10c92c8bdd473fae4e356c85d
Banniroy <matinadekola33@gmail.com>
2023-07-13 12:12:29 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState, useEffect, lazy, Suspense, useRef } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { Route, Routes} from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Loading from './components/Loading-state/Loading';
 import Missing from './components/Missing';
-import { database, storage, auth } from './firebase/firebaseSDK';
-import { ref, onValue, limitToFirst, query } from 'firebase/database';
-import { ref as storageRef, getDownloadURL } from 'firebase/storage';
-import toast, { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 
 // Lazy loading componenets
 const Home = lazy(() => import('./components/HomeList-components/Home'));
